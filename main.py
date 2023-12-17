5e43d635913db3d151d69f992df185c71355cdc7
Cyril <cyril.turpel@area21.eu>
2023-12-17 22:04:07 +0100

    added content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -15,3 +16,26 @@ with col2:
     sxiduhkgcvrotlhbonrflxihcbgcdoriflxgn!
     """
     st.info(content)
+
+content2 = """
+eirfkhjnwelrnfvleisdutfnvliseuthnvlresuitbfnvilrsutbkvfnlsiudbkfvnslidtukbfvsdvlkfnj
+"""
+st.info(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.3, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.image("images/" + row["image"])
+        st.write(row["description"])
+        st.write(f"[Dat as e Link]({row['url']})")
+
+with col4:
+     for index, row in df[10:].iterrows():
+         st.header(row["title"])
+         st.image("images/" + row["image"])
+         st.write(row["description"])
+         st.write(f"[Dat as e Link]({row['url']})")
